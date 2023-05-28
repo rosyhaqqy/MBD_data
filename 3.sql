725a7913bcf6024c534503dd0f0031fb5c2114ab
rosyhaqqy <111132143+rosyhaqqy@users.noreply.github.com>
2023-05-28 09:40:41 +0700

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -1,4 +1,14 @@
-SELECT Playlist.Name, COUNT(PlaylistTrack.TrackId) AS SongCount
-FROM Playlist
-JOIN PlaylistTrack ON Playlist.PlaylistId = PlaylistTrack.PlaylistId
-GROUP BY Playlist.PlaylistId, Playlist.Name;
+SELECT 'Album' AS Type, SUM(Invoice.Total) AS TotalSales
+FROM Album
+JOIN Artist ON Album.ArtistId = Artist.ArtistId
+JOIN Track ON Album.AlbumId = Track.AlbumId
+JOIN InvoiceLine ON Track.TrackId = InvoiceLine.TrackId
+JOIN Invoice ON InvoiceLine.InvoiceId = Invoice.InvoiceId
+GROUP BY Type
+UNION
+SELECT 'Track' AS Type, SUM(Invoice.Total) AS TotalSales
+FROM Track
+JOIN Genre ON Track.GenreId = Genre.GenreId
+JOIN InvoiceLine ON Track.TrackId = InvoiceLine.TrackId
+JOIN Invoice ON InvoiceLine.InvoiceId = Invoice.InvoiceId
+GROUP BY Type;
